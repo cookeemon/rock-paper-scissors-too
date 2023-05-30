f916205a2d4fdd7af9f2010be98c1ddaa210f8c8
cookeemon <50350262+cookeemon@users.noreply.github.com>
2023-05-29 22:38:27 -0400

    Upgraded UI to play using clickable images
    I have greatly improved the UI for the game. The UI consists of three
    images of rock paper and scissors which can be clicked to play.
    Additionally, the selection for the program automatically using a time
    interval. When the player chooses an image to click, the interval stops
    and displays a random pick by the program. Ultimately, the scores are
    updated and a winner is selected when either the player or the program
    reached 5 points. A message pops up and the player can try again.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rock-paper-scissors-too
+
+Assets:
+{
+    Author: Clker-Free-Vector-Images
+    Website: Pixabay.com
+}
+
+{
+    Author: dueg-oth
+    Website: Pixabay.com
+}
--- main.js
+++ main.js
@@ -1,105 +1,161 @@
-const buttons = document.querySelectorAll('button');
-const playerScore = document.querySelector('.playerScore');
-const computerScore = document.querySelector('.computerScore');
-const winner = document.querySelector('.winner');
-console.log(playerScore);
+/* Functions and variables for automatic image shuffling */
+const cmpImgContainer = document.querySelector('.cmp-selection-container');
+const cmpImages = Array.from(cmpImgContainer.getElementsByTagName('img'));
+let currentIndex = 0;
+let shufflingInterval;
+startShuffling();
 
-buttons.forEach((button) => {
-    button.addEventListener('click', playRound);
-    
+function startShuffling() {
+    shufflingInterval = setInterval(changeImage, 300);
+}
+  
+function changeImage() {
+    cmpImages.forEach((img, index) => {
+        if (index === currentIndex) {
+            img.classList.remove('hidden');
+        } else {
+            img.classList.add('hidden');
+        }
+    });
+    currentIndex = (currentIndex + 1) % cmpImages.length;
+}
+
+function stopShuffling() {
+    clearInterval(shufflingInterval);
+    setTimeout(startShuffling, 800);
+};
+
+/* Function for getting and displaying a random choice by the program*/
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * cmpImages.length);
+    const randomImage = cmpImages[randomIndex];
+    displayComputerChoice(randomIndex);
+    const computerChoice = randomImage.classList.toString().split(' ');
+    return computerChoice[0];
+}
+
+function displayComputerChoice(randomIndex) {
+    cmpImages.forEach((img, index) => {
+        if (randomIndex === index) {
+            img.classList.remove('hidden');
+        } else {
+            img.classList.add('hidden');
+        }
+    });
+}
+
+/* Functions and Variables for player and their selection*/
+
+const userSelection = document.querySelectorAll('.user-selection-container img');
+
+userSelection.forEach(image => {
+    image.addEventListener('click', playerClicked);
 });
 
-function getComputerChoice() {
-    const RPS = ["Rock", "Paper", "Scissors"];
-    let randomChoice = Math.floor(Math.random() * RPS.length);
-    return RPS[randomChoice];
+function playerClicked(e) { 
+        console.log('Click')
+        stopShuffling();
+        stopPlayerClicking();
+        const roundResult = playRound(e.target.className);
+        updateScore(roundResult);
+        gameEnd();
 }
 
-function getPlayerChoice(e) {
-    if(e.target.className == 'rockBtn') return 'rock';
-    if(e.target.className == 'paperBtn') return 'paper';
-    if(e.target.className == 'scissorsBtn') return 'scissors';
+function stopPlayerClicking() {
+    userSelection.forEach(image => {
+        image.removeEventListener('click', playerClicked);
+        image.style.pointerEvents = 'none'
+    });
+    setTimeout(() => {
+        userSelection.forEach(image => {
+          image.addEventListener('click', playerClicked);
+          image.style.pointerEvents = 'auto'
+        });
+    }, 1000);
 }
 
-const updatePlayerScore = () => {
-    let currScore = parseInt(playerScore.textContent);
-    currScore += 1;
-    playerScore.textContent = currScore;
-    updateWinner();
-}
+const tryAgainBtn = document.querySelector('.try-again button');
 
-const updateComputerScore = () => {
-    let currScore = parseInt(computerScore.textContent);
-    currScore += 1;
-    computerScore.textContent = currScore;
-    updateWinner();
-}
+tryAgainBtn.addEventListener('click', () => {
+    playerScore.textContent = 0;
+    cmpScore.textContent = 0;
+    gamePanel.classList.toggle('hidden');
+    tryAgainPanel.classList.toggle('hidden');
+});
 
-const updateWinner = () => {
-    if (+(playerScore.textContent) == 5) {
-        winner.textContent = 'You Win!' 
-        resetGame();
-        }
+const roundResultDisplay = document.querySelector('.roundResultDisplay');
+const playerScore = document.querySelector('.player-score');
+const cmpScore = document.querySelector('.cmp-score');
 
-    if (+(computerScore.textContent) == 5) {
-        winner.textContent = 'You Lose!'
-        resetGame();
+function updateScore(roundResult) {
+    if      (roundResult == 'draw')roundResultDisplay.textContent = 'Round Draw';
+    else if (roundResult == 'win' ) {
+        roundResultDisplay.textContent = 'Round Win';
+        playerScore.textContent = +(playerScore.textContent)+ 1;
     }
+    else if (roundResult == 'lost') {
+        roundResultDisplay.textContent= 'Round Lost';
+        cmpScore.textContent = +(cmpScore.textContent)+ 1;
+    }
+        
 }
 
-const resetGame = () => {
-    playerScore.textContent   = 0;
-    computerScore.textContent = 0;
-    winner.textContent        = 'None';
-}
-
-function playRound(e) {
-    let computerSelected = getComputerChoice();
-    let playerSelected   = getPlayerChoice(e);
-
-    console.log(`You Chose: ${playerSelected}`);
-    console.log(`Computer Chose: ${computerSelected}`);
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
 
     switch (true) {
         
-        case (playerSelected.toLowerCase() === computerSelected.toLowerCase()) : {
-            break;
+        case (playerChoice === computerChoice) : {
+            return 'draw';
            }
 
-        case (playerSelected.toLowerCase()   === "rock"
-           && computerSelected.toLowerCase() === "scissors") : {
-            updatePlayerScore();
-            break;
+        case (playerChoice   === "rock"
+           && computerChoice === "scissors") : {
+            return 'win';
            }
         
-        case (playerSelected.toLowerCase()     === "scissors"
-           && computerSelected.toLowerCase()   === "paper") : {
-            updatePlayerScore();
-            break;
+        case (playerChoice     === "scissors"
+           && computerChoice   === "paper") : {
+            return 'win';
            }
 
-        case (playerSelected.toLowerCase()     === "paper"
-           && computerSelected.toLowerCase()   === "rock") : {
-            updatePlayerScore();
-            break;
+        case (playerChoice     === "paper"
+           && computerChoice   === "rock") : {
+            return 'win';
            }
         
-        case (playerSelected.toLowerCase()     === "scissors"
-           && computerSelected.toLowerCase()   === "rock") : {
-            updateComputerScore();
-            break;
+        case (playerChoice     === "scissors"
+           && computerChoice   === "rock") : {
+            return 'lost';
            }
 
-        case (playerSelected.toLowerCase()     === "paper"
-           && computerSelected.toLowerCase()   === "scissors") : {
-            updateComputerScore();
-            break;
+        case (playerChoice     === "paper"
+           && computerChoice   === "scissors") : {
+            return 'lost';
            }
 
-        case (playerSelected.toLowerCase()     === "rock"
-           && computerSelected.toLowerCase()   === "paper") : {
-            updateComputerScore();
-            break;
+        case (playerChoice     === "rock"
+           && computerChoice   === "paper") : {
+            return 'lost';
            }
+    }
+}
+
+const gamePanel = document.querySelector('.game-panel');
+const tryAgainPanel = document.querySelector('.try-again');
+const tryAgainPanelText = document.querySelector('.try-again p');
+
+function gameEnd() {
+    if(playerScore.textContent == '5') {
+        console.log(playerScore);
+        tryAgainPanelText.textContent = 'Victory!!!'
+        gamePanel.classList.toggle('hidden');
+        tryAgainPanel.classList.toggle('hidden');
+    }
+    else if (cmpScore.textContent == '5') {
+        console.log(cmpScore);
+        tryAgainPanelText.textContent = 'Lost Against a Dumb Program. Ha Ha Ha!'
+        gamePanel.classList.toggle('hidden');
+        tryAgainPanel.classList.toggle('hidden');
     }
 }
